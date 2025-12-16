027e365113777ddc1c1d578b54df248b34fe3475
GurcyberSingh99 <singhgurcyber@gmail.com>
2025-12-16 13:44:13 +0530

    Updated Main.java layout

--- src/main/java/com/currency/Main.java
+++ src/main/java/com/currency/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        Label title = new Label("ABC Converter");
+        Label title = new Label("abc Converter");
         
         TextField amountField = new TextField();
         amountField.setPromptText("Enter amount in INR");
